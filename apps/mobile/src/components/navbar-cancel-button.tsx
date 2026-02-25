4e953ba61e382d7bc15d78495bf96118b03a04c1
Jan Hofmann <jah@heise.de>
2026-02-25 21:52:34 +0100

    feat: Nachfrage etwas nativer

--- apps/mobile/src/components/navbar-cancel-button.tsx
+++ apps/mobile/src/components/navbar-cancel-button.tsx
@@ -3,17 +3,17 @@ import { SymbolView } from 'expo-symbols'
 import { type FC, useEffect, useState } from 'react'
 import { Alert, PlatformColor } from 'react-native'
 import { Pressable } from 'react-native-gesture-handler'
+import * as DropdownMenu from 'zeego/dropdown-menu'
+import { colors } from './colors'
 
 export interface NavbarCancelProps {
   preventRemove: boolean
-  disabled?: boolean
   onCancel: () => void
 }
 
 export const NavbarCancelButton: FC<NavbarCancelProps> = ({
   preventRemove,
   onCancel,
-  disabled,
 }) => {
   const [_preventRemove, _setPreventRemove] = useState(preventRemove)
 
@@ -46,15 +46,53 @@ export const NavbarCancelButton: FC<NavbarCancelProps> = ({
     )
   }
 
-  return (
-    <Pressable onPress={handleOnCancel} disabled={disabled}>
-      <SymbolView
-        name="multiply"
-        size={24}
-        type="monochrome"
-        tintColor={PlatformColor('label')}
-        style={{ margin: 6 }}
-      />
-    </Pressable>
-  )
+  if (preventRemove) {
+    return (
+      <DropdownMenu.Root>
+        <DropdownMenu.Trigger>
+          <Pressable>
+            <SymbolView
+              name="multiply"
+              size={24}
+              style={{ margin: 6 }}
+              tintColor={colors.label.primary}
+            />
+          </Pressable>
+        </DropdownMenu.Trigger>
+
+        <DropdownMenu.Content>
+          <DropdownMenu.Label key="cancel">
+            Möchtest du die Änderungen wirklich verwerfen?
+          </DropdownMenu.Label>
+
+          <DropdownMenu.Item
+            key="discard"
+            destructive
+            style={{ fontWeight: 700 }}
+            onSelect={() => {
+              onCancel()
+              _setPreventRemove(false)
+              onCancel()
+            }}
+          >
+            <DropdownMenu.ItemTitle>
+              Änderungen verwerfen
+            </DropdownMenu.ItemTitle>
+          </DropdownMenu.Item>
+        </DropdownMenu.Content>
+      </DropdownMenu.Root>
+    )
+  } else {
+    return (
+      <Pressable onPress={handleOnCancel}>
+        <SymbolView
+          name="multiply"
+          size={24}
+          type="monochrome"
+          tintColor={PlatformColor('label')}
+          style={{ margin: 6 }}
+        />
+      </Pressable>
+    )
+  }
 }
